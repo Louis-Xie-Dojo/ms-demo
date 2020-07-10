a3f7b508042e98fbc6df1675b95f5669320a19eb
dclouis <dclouis@foxmail.com>
2020-07-10 20:56:00 +0800

    [Lou] rename service

---
+++ serviceB/src/main/java/com/horror/item/ServiceBApplication.java
@@ -0,0 +1,14 @@
+package com.horror.item;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class ServiceBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceBApplication.class, args);
+	}
+
+}
