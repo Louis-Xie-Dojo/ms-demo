43be9defc4c1d7afa46a4866d39c2503b837a9c4
dclouis <dclouis@foxmail.com>
2020-07-12 10:15:31 +0800

    [Lou] add client side

---
+++ client/src/test/java/com/horror/client/ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.horror.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
